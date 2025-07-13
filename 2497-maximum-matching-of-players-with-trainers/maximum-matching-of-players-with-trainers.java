3f81385dbf9907820c5f3cc1be50f756f2bfae92
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2025-07-13 09:52:29 +0530

    Time: 5 ms (100.00%) | Memory: 64.9 MB (34.15%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,7 +1,18 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players);
-        Arrays.sort(trainers);
+        Thread t1=new Thread(()->Arrays.sort(players));
+        Thread t2=new Thread(()->Arrays.sort(trainers));
+        // Arrays.sort(players);
+        // Arrays.sort(trainers);
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+        }
+        catch(Exception e){
+            // e.printStackTrace();
+        }
         int n=players.length,m=trainers.length;
         int count=0,i=0,j=0;
         while(i<n &&j<m){
