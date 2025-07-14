141cc686da14e6a050b0d29f798e30c53e79e340
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2025-07-14 10:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (88.72%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,38 +1,10 @@
-class Node{
-    int val;
-    Node prev;
-    Node next;
-    Node(int val){
-        this.val=val;
-    }
-    static int func(int n,int k){
-        Node head =new Node(1);
-        Node prev=head;
-        for(int i=2;i<=n;i++){
-            Node curr=new Node(i);
-            curr.prev=prev;
-            prev.next=curr;
-            prev=curr;
-        }
-        head.prev=prev;
-        prev.next=head;
-        Node curr=head;
-        while(curr.next!=curr){
-            for(int i=1;i<k;i++){
-                curr=curr.next;
-            }
-            curr.prev.next=curr.next;
-            curr.next.prev=curr.prev;
-            curr=curr.next;
-        }
-        return curr.val;
-
-    }
-}
 class Solution {
+    static int helper(int n,int k){
+        if(n==1)    return 0;
+        return (helper(n-1,k)+k)%n;
+    }
     public int findTheWinner(int n, int k) {
-        // LinkedList<Integer> llist=new LinkedList<>();
-        return Node.func(n,k);
-
+        int ind=helper(n,k);
+        return ind+1;
     }
 }
