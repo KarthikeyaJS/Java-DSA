f13170862750ec7c22bd5c1d8df1a8e582d23263
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2025-07-24 14:52:08 +0530

    Time: 66 ms (86.12%) | Memory: 61.4 MB (11.88%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -6,12 +6,12 @@ class Solution {
         int l,maxi=0;
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])>k){
+            // if(map.get(nums[i])>k){
                 while(map.get(nums[i])>k){
                     map.put(nums[j],map.get(nums[j])-1);
                     j++;
                 }
-            }
+            // }
             l=i-j+1;
             maxi=Math.max(maxi,l);
         }
