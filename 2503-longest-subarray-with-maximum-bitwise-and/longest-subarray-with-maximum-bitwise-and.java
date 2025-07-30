5c82afc1556070501b3ef72577804f3380b0060f
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2025-07-30 10:18:46 +0530

    Time: 3 ms (82.57%) | Memory: 61 MB (48.13%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -5,15 +5,15 @@ class Solution {
         for(int i:nums){
             maxi=Math.max(maxi,i);
         }
-        int count=1;
-        int maxCount=1;
-        for(int i=0;i<n;i++){
-            if(i+1<n && maxi==nums[i] && nums[i]==nums[i+1]){
+        int count=0;
+        int maxCount=0;
+        for(int i:nums){
+            if(i==maxi){
                 count++;
                 maxCount=Math.max(maxCount,count);
-            } 
+            }
             else{
-                count=1;
+                count=0;
             }
         }
         return maxCount;
