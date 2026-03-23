45ed30d7289762e37e30957273616728d168d1cc
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-03-23 18:49:50 +0530

    Time: 1 ms (64.06%) | Memory: 43.2 MB (62.55%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        StringBuilder sb=new StringBuilder();
+        String first=strs[0];
+        String last=strs[strs.length-1];
+        for(int i=0;i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i)==last.charAt(i))
+                sb.append(first.charAt(i));
+            else{
+                break;
+            }
+        }
+        return sb.toString();
+    }
+}
+
