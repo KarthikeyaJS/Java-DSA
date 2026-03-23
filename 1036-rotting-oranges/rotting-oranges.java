2a1746aeca19658fbc5d2fe685a7cd2739596402
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-03-23 19:27:42 +0530

    Time: 2 ms (90.48%) | Memory: 44.1 MB (66.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,32 +1,32 @@
 class Solution {
-
-    public int orangesRotting(int[][] visited) {
-        int m=visited.length;
-        int n=visited[0].length;
-        // int[][] visited=grid;
+    public int orangesRotting(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
         Queue<int[]> q=new LinkedList<>();
         int countFresh=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]==2)    q.offer(new int[]{i,j});
-                if(visited[i][j]==1)    countFresh++;
+                if(grid[i][j]==1)   countFresh++;
+                if(grid[i][j]==2)   q.offer(new int[]{i,j});
             }
         }
         if(countFresh==0)   return 0;
         if(q.isEmpty())     return -1;
+
         int minutes=-1;
-        int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dirs={{1,0},{0,1},{-1,0},{0,-1}};
+
         while(!q.isEmpty()){
             int size=q.size();
-            while(size-- >0){
+            while(size-->0){
                 int[] cell=q.poll();
                 int x=cell[0];
                 int y=cell[1];
-                for(int[] d:directions){
+                for(int[] d:dirs){
                     int i=x+d[0];
                     int j=y+d[1];
-                    if(i>=0&& i<m && j>=0 && j<n && visited[i][j]==1){
-                        visited[i][j]=2;
+                    if(i>=0 &&i<m &&j>=0&&j<n && grid[i][j]==1){
+                        grid[i][j]=2;
                         countFresh--;
                         q.offer(new int[]{i,j});
                     }
