8dbe2c80cfab10c3985ff232bd64501b90aa37c9
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-04-06 17:20:41 +0530

    Time: 2 ms (59.27%) | Memory: 43.9 MB (39.66%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n=code.length;
-        int[] result=new int[n];
-        if(k==0)    return result;
-
-        int wSum=0;
-        int start=(k>0)?1:n+k;
-        int end=(k>0)?k:n-1;
-        for(int i=start;i<=end;i++){
-            wSum=wSum+code[i];
-        }
+        int[] res=new int[n];
+        if(k==0)    return res;
         for(int i=0;i<n;i++){
-            result[i]=wSum;
-            wSum=wSum - code[(start+i)%n];
-            wSum=wSum + code[(end+i+1)%n];
+            if(k>0){
+                for(int j=1;j<=k;j++){
+                    res[i]=res[i]+code[(i+j)%n];
+                }
+            }
+            else{
+                for(int j=1;j<=-k;j++){
+                    res[i]=res[i]+code[(n+i-j)%n];
+                }
+            }
         }
-        return result;
-
+        return res;
     }
 }
