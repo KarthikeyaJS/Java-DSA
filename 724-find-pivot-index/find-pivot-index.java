317865b1c2d00ca92062f0e7fedf83e84a85b162
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-04-06 21:57:05 +0530

    Time: 1 ms (98.65%) | Memory: 47.7 MB (14.84%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,19 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n=nums.length;
-        int total=0;
-        for(int i:nums){
-            total=total+i;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        left[0]=nums[0];
+        for(int i=1;i<n;i++){
+            left[i]=left[i-1]+nums[i];
         }
-        int left=0;
+        right[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]+nums[i];
+        }
         for(int i=0;i<n;i++){
-            int right=total-nums[i]-left;
-            if(right==left){
-                return i;
-            } 
-            left=left+nums[i];
+            if(left[i]==right[i])   return i;
         }
         return -1;
-        
     }
 }
