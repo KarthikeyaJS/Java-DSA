416e847ac3b000e4acdd66d9d10d96f85ecc5acd
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-04-10 11:14:33 +0530

    Time: 2 ms (87.46%) | Memory: 44.4 MB (59.16%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int n=nums.length;
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    for(int k=j+1;k<n;k++){
+                        if(nums[j]==nums[k]){
+                            ans=Math.min(ans,k-i);
+                        }
+                    }
+                }
+            }
+
+        }
+        return ans==Integer.MAX_VALUE?-1:ans*2;
+    }
+}
