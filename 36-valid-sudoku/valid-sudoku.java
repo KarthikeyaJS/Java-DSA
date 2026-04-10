677370d1cb4a76a0c15aa8f1e1afe01e2aa9227c
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-04-10 12:42:25 +0530

    Time: 2 ms (66.14%) | Memory: 46.5 MB (62.04%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -4,16 +4,16 @@ class Solution {
             HashSet<Character> row=new HashSet<>();
             HashSet<Character> col=new HashSet<>();
             for(int j=0;j<9;j++){
-                if(board[i][j]!='.' && !row.add(board[i][j])) return false;
-                if(board[j][i]!='.' && !col.add(board[j][i])) return false;
+                if(board[i][j]!='.' && !row.add(board[i][j]))   return false;
+                if(board[j][i]!='.' && !col.add(board[j][i]))   return false;
             }
-        }
+        }        
         for(int i=0;i<9;i+=3){
             for(int j=0;j<9;j+=3){
                 HashSet<Character> box=new HashSet<>();
-                for(int sr=0;sr<3;sr++){
-                    for(int sc=0;sc<3;sc++){
-                        char ch=board[sr+i][sc+j];
+                for(int r=0;r<3;r++){
+                    for(int c=0;c<3;c++){
+                        char ch=board[i+r][j+c];
                         if(ch!='.' && !box.add(ch)) return false;
                     }
                 }
@@ -22,3 +22,4 @@ class Solution {
         return true;
     }
 }
+
