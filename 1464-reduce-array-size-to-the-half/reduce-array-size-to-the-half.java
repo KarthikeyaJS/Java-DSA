8c82ad4a093c49704ddded5746a2ce3c73c0835c
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-04-17 20:38:36 +0530

    Time: 32 ms (87.86%) | Memory: 87.4 MB (61.85%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -1,46 +1,23 @@
-// store freq
-// set-> add elements
-// find point when arr length becomes <=half
-// return size of set
-
-class Pair{
-    int k;
-    int v;
-    Pair(int k,int v){
-        this.k=k;
-        this.v=v;
-    }
-}
 class Solution {
     public int minSetSize(int[] arr) {
-        List<Pair> list=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
-        
         for(int i:arr){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        int n=arr.length;
-        HashSet<Integer> set=new HashSet<>();
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            Pair p=new Pair(entry.getKey(),entry.getValue());
-            list.add(p);
+        int[] freq=new int[map.values().size()];
+        int i=0;
+        for(int f:map.values()){
+            freq[i++]=f;
         }
-        Collections.sort(list, new Comparator<Pair>(){
-            @Override
-            public int compare(Pair p1,Pair p2){
-                return p2.v-p1.v;
-            }
-        });
-        int count=n;
-        for(Pair i:list){
-            set.add(i.k);
-            count=count-i.v;
-            if(count<=n/2)  return set.size();
+        Arrays.sort(freq);
+        i=freq.length-1;
+        int half=arr.length/2;
+        int count=0;
+        int remove=0;
+        while(remove<half){
+            count+=1;
+            remove+=freq[i--];
         }
-        
-        return 0;
-
+        return count;
     }
 }
-
-
