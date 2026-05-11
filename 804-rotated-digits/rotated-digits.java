1764594d1e1c2cec43770372a31871c8a07bd446
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-05-11 11:38:35 +0530

    Time: 3 ms (93.89%) | Memory: 42 MB (72.08%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,35 +1,22 @@
-//main condition->if 3,4,7 return false;
-//->remaining all numbers are valid
-//->to be diff from x->
-
-//  2   5 6   9
-
-//88828->88858
-
-//1000->10005
-
-//1801809->1801806
-
-//3222
-
 class Solution {
-    static boolean isValid(int n){
+    static boolean isGood(int n){
         boolean found=false;
         while(n>0){
             int x=n%10;
-            if(x==3||x==4||x==7)    return false;
+            if(x==3|| x==4||x==7)   return false;
             if(x==2||x==5||x==6||x==9)  found=true;
             n=n/10;
         }
         return found;
-
     }
     public int rotatedDigits(int n) {
+
         int count=0;
-        for(int i=0;i<=n;i++){
-            if(isValid(i))  count++;
+        for(int i=1;i<=n;i++){
+            if(isGood(i)){
+                count++;
+            }
         }
         return count;
     }
 }
-
