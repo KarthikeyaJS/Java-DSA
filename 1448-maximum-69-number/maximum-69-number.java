c61a40bbf9ee32bd1f67a8af499e40b7199df031
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-05-13 14:50:06 +0530

    Time: 1 ms (82.53%) | Memory: 42.4 MB (29.23%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maximum69Number (int num) {
         char[] arr=String.valueOf(num).toCharArray();
-        for(int i=0;i<arr.length;i++){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
             if(arr[i]=='6'){
                 arr[i]='9';
                 break;
