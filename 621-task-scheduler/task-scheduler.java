4d0829cd886b6b4c35b5bf83cf0019814f7f39cb
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-05-13 15:29:42 +0530

    Time: 22 ms (49.63%) | Memory: 47.4 MB (97.47%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -2,15 +2,15 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         HashMap<Character,Integer> map=new HashMap<>();
         int maxi=0;
-        int maxCount=0;
+        int count=0;
         int total=tasks.length;
         for(char ch:tasks){
             map.put(ch,map.getOrDefault(ch,0)+1);
             maxi=Math.max(maxi,map.get(ch));
         }
         for(int i:map.values()){
-            if(maxi==i)    maxCount++;
+            if(maxi==i) count++;
         }
-        return Math.max(total,(maxi-1)*(n+1)+maxCount );
+        return Math.max(total,(maxi-1)*(n+1)+count);
     }
 }
