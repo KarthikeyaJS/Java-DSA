cbf7d9d6bc0840d65e31fe7581e98d3c9680ef1f
KarthikeyaJS <151557894+KarthikeyaJS@users.noreply.github.com>
2026-06-10 12:11:22 +0530

    Time: 3 ms (59.46%) | Memory: 19.1 MB (94.28%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,8 +1,23 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        s = set()
-        for num in arr:
-            if num*2 in s or (num % 2 == 0 and num // 2 in s):
-                return True 
-            s.add(num)
+        arr.sort()
+        for i in range(len(arr)):
+            if self.binary_search(arr,arr[i]*2,i):
+                return True
         return False
+
+    def binary_search(self,arr,x,idx):
+        l=0
+        r=len(arr)-1
+        while(l<=r):
+            m=l+(r-l)//2
+            if arr[m]<x:
+                l=m+1
+            elif arr[m]>x:
+                r=m-1
+            else:
+                if m!=idx:
+                    return True
+                return False
+        return False
+    
